c8a2763c7677c0d0826b86cf750b35e5d99e2698
fmikaelian <39884124+fmikaelian@users.noreply.github.com>
2019-02-16 12:30:20 +0100

    Upload weights and metrics and update download.py script #21

--- cdqa/pipeline/download.py
+++ cdqa/pipeline/download.py
@@ -1,11 +1,43 @@
+import os
 import wget
+import requests
+from github import Github
 
-squad_urls = [
-    'https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json',
-    'https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json',
-]
+def main():
+    squad_urls = [
+        'https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json',
+        'https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json',
+    ]
 
-for squad_url in squad_urls:
-    wget.download(url=squad_url, out='data')
+    for squad_url in squad_urls:
+        wget.download(url=squad_url, out='data')
 
-wget.download(url='https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py')
+    wget.download(url='https://github.com/allenai/bi-att-flow/blob/master/squad/evaluate-v1.1.py')
+
+    token = os.environ['token']
+    g = Github(token)
+
+    repo = g.get_repo('fmikaelian/cdQA')
+    release = repo.get_release('bert_qa_squad_v1.1')
+    assets = release.get_assets()
+
+    headers = {
+        'Authorization': 'token {}'.format(token),
+        'Accept': 'application/octet-stream'
+    }
+
+    for asset in assets:
+        print(asset.name, asset.url)
+        if os.path.splitext(asset.name)[1] == '.bin':
+            directory = 'models'
+        else:
+            directory = 'logs'
+        response = requests.get(asset.url, headers=headers)
+        if not os.path.exists(os.path.join(directory, release.tag_name)):
+            os.makedirs(os.path.join(directory, release.tag_name))
+        with open(os.path.join(directory, release.tag_name, asset.name), 'wb') as handle:
+            for block in response.iter_content(1024):
+                handle.write(block)
+
+if __name__ == '__main__':
+    main()
