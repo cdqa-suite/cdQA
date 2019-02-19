ec0f199117121be190128b8978de4c0bf07b1215
Felix MIKAELIAN <39884124+fmikaelian@users.noreply.github.com>
2019-02-19 11:16:37 +0100

    Find a robust method to get articles paragraphs #1

--- examples/run_converter.py
+++ examples/run_converter.py
@@ -1,8 +1,10 @@
 import pandas as pd
-from cdqa.utils.converter import df2squad
+from cdqa.utils.converter import df2squad, filter_paragraphs
 from ast import literal_eval
 
 # https://stackoverflow.com/questions/32742976/how-to-read-a-column-of-csv-as-dtype-list-using-pandas
 df = pd.read_csv('bnpp_newsroom_v1.0.csv', converters={'paragraphs': literal_eval})
+
+df['paragraphs'] = df['paragraphs'].apply(filter_paragraphs)
 
 json_data = df2squad(df=df, version='v2.0', output_dir='./')
