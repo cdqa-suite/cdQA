ab2e007e06d5f89b3f2af4963c278726014c1c1f
Felix MIKAELIAN <39884124+fmikaelian@users.noreply.github.com>
2019-02-19 11:00:17 +0100

    Update run_converter.py

--- examples/run_converter.py
+++ examples/run_converter.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from cdqa.utils.converter import df2squad
+from ast import literal_eval
 
-df = pd.read_csv('data.csv')
+# https://stackoverflow.com/questions/32742976/how-to-read-a-column-of-csv-as-dtype-list-using-pandas
+df = pd.read_csv('bnpp_newsroom_v1.0.csv', converters={'paragraphs': literal_eval})
 
 json_data = df2squad(df=df, version='v2.0', output_dir='./')
