13badce13068d5d4d7e1d97cbf264f0593beadda
Felix MIKAELIAN <39884124+fmikaelian@users.noreply.github.com>
2019-02-26 16:38:56 +0100

    Adapt retriever.py to BNP Paribas Newsroom dataset v.1.0 #45

--- cdqa/retriever/tfidf_doc_ranker.py
+++ cdqa/retriever/tfidf_doc_ranker.py
@@ -8,22 +8,26 @@ def train_document_retriever(corpus):
     tfidf_matrix = vectorizer.fit_transform(corpus)
     return vectorizer, tfidf_matrix
 
-def predict_document_retriever(question, vectorizer, tfidf_matrix, top_n, verbose=True):
+def predict_document_retriever(question, paragraphs, vectorizer, tfidf_matrix, top_n, metadata, verbose=True):
+    t0 = time.time()
     question_vector = vectorizer.transform([question])
     scores = pd.DataFrame(tfidf_matrix.dot(question_vector.T).toarray())
-    closest_docs_indices = scores.sort_values(by=0, ascending=False).index[:top_n].to_list()
+    closest_docs_indices = scores.sort_values(by=0, ascending=False).index[:top_n].values
+    
+    # inspired from https://github.com/facebookresearch/DrQA/blob/50d0e49bb77fe0c6e881efb4b6fe2e61d3f92509/scripts/reader/interactive.py#L63
     if verbose:
-        process(closest_docs_indices)
-    return(closest_docs_indices)
+        rank = 1
+        table = prettytable.PrettyTable(['rank', 'index', 'title'])
+        for i in range(len(closest_docs_indices)):
+            index = closest_docs_indices[i]
+            if paragraphs:
+                article_index = paragraphs[int(index)]['index']
+                title = metadata.iloc[int(article_index)]['title']
+            else:
+                title = metadata.iloc[int(index)]['title']
+            table.add_row([rank, index, title])
+            rank+=1
+        print(table)
+        print('Time: {} seconds'.format(round(time.time() - t0, 5)))
 
-def process(closest_docs_indices):
-    t0 = time.time()
-    rank = 1
-    table = prettytable.PrettyTable(['rank', 'index', 'title'])
-    for i in range(len(closest_docs_indices)):
-        index = closest_docs_indices[i]
-        title = df.iloc[int(index)]['title']
-        table.add_row([rank, index, title])
-        rank+=1
-    print(table)
-    print('Time: {} seconds'.format(time.time() - t0))
+    return(closest_docs_indices)
