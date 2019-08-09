d700ca5ab560e9db2392f16a0b342771e71ee2ac
Claudia C <claudia.cao.pro@gmail.com>
2019-08-09 14:40:16 +0200

    debug test, add fixture and class

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,44 +1,47 @@
 import os
 import wget
+import pytest
 from cdqa.utils.converters import pdf_converter, md_converter
 
-def download_test_assets(type):
-  directory = 'data/{}/'.format(type)
-  if type == 'pdf':
+@pytest.fixture(scope="session")
+def download_test_assets(tmpdir_factory):
     assets_urls = [
+        # PDF
         'https://invest.bnpparibas.com/documents/1q19-pr-12648',
         'https://invest.bnpparibas.com/documents/4q18-pr-18000',
-        'https://invest.bnpparibas.com/documents/4q17-pr'
-    ]
-  elif type == 'md':
-    assets_urls = [
+        'https://invest.bnpparibas.com/documents/4q17-pr',
+        # MD
         'https://raw.githubusercontent.com/cdqa-suite/cdQA/master/README.md',
         'https://raw.githubusercontent.com/huggingface/pytorch-transformers/master/docs/source/quickstart.md',
         'https://raw.githubusercontent.com/huggingface/pytorch-transformers/master/docs/source/migration.md'
     ]
-  print('\nDownloading {} assets...'.format(type))
 
-  if not os.path.exists(directory):
-      os.makedirs(directory)
-  for url in assets_urls:
-      wget.download(url=url, out=directory)
+    print('\nDownloading assets...')
+    fn = tmpdir_factory.mktemp('assets_data')
+    for url in assets_urls:
+        wget.download(url=url, out=str(fn))
+    return fn
 
-def test_converter(converter, type):
-    download_test_assets(type)
-    df = converter(directory_path='data/{}/'.format(type))
-    errors = []
+class Test_converter:
+    @pytest.fixture(autouse=True)
+    def get_assets_folder(self, download_test_assets):
+        self.assets_folder = download_test_assets
 
-    # replace assertions by conditions
-    if not df.shape == (3, 2):
-        errors.append('resulting dataframe has unexpected shape.')
-    if not (isinstance(df.paragraphs[0][0], str) and isinstance(df.title[0], str)):
-        errors.append('paragraph column content has wrong format.')
+    def df_converter_check(self, df):
+        errors = []
+        # replace assertions by conditions
+        if not df.shape == (3, 2):
+            errors.append('resulting dataframe has unexpected shape.')
+        if not (isinstance(df.paragraphs[0][0], str) and isinstance(df.title[0], str)):
+            errors.append('paragraph column content has wrong format.')
 
-    # assert no error message has been registered, else print messages
-    assert not errors, "errors occured:\n{}".format("\n".join(errors))
+        # assert no error message has been registered, else print messages
+        assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-def test_pdf_converter():
-    test_converter(pdf_converter, type='pdf')
+    def test_md_converter(self):
+        df = md_converter(directory_path=self.assets_folder)
+        self.df_converter_check(df)
 
-def test_md_converter():
-    test_converter(md_converter, type='md')
+    def test_pdf_converter(self):
+        df = pdf_converter(directory_path=self.assets_folder)
+        self.df_converter_check(df)
