cff2d4404953d58880a2b5a3fcca52941a3e53cb
André <43521764+andrelmfarias@users.noreply.github.com>
2019-08-13 09:51:51 +0100

    Included other paramaters of tfidf_vectorizer in Retriever  (#233)
    * Included other paramaters of tfidf_vectorizer in Retriever and improved class documentation
    
    * fixed token_pattern
    
    * Update setup.py

--- cdqa/retriever/tfidf_sklearn.py
+++ cdqa/retriever/tfidf_sklearn.py
@@ -12,12 +12,46 @@ class TfidfRetriever(BaseEstimator):
 
     Parameters
     ----------
-    ngram_range : bool, optional
-        [description] (the default is False)
-    max_df : bool, optional
-        [description] (the default is False)
-    stop_words : bool, optional
-        [description] (the default is False)
+    lowercase : boolean
+        Convert all characters to lowercase before tokenizing. (default is True)
+    preprocessor : callable or None
+        Override the preprocessing (string transformation) stage while preserving
+        the tokenizing and n-grams generation steps. (default is None)
+    tokenizer : callable or None
+        Override the string tokenization step while preserving the preprocessing
+        and n-grams generation steps (default is None)
+    stop_words : string {‘english’}, list, or None
+        If a string, it is passed to _check_stop_list and the appropriate stop
+        list is returned. ‘english’ is currently the only supported string value.
+        If a list, that list is assumed to contain stop words, all of which will
+        be removed from the resulting tokens.
+        If None, no stop words will be used. max_df can be set to a value in the
+        range [0.7, 1.0) to automatically detect and filter stop words based on
+        intra corpus document frequency of terms.
+        (default is None)
+    token_pattern : string
+        Regular expression denoting what constitutes a “token”. The default regexp
+        selects tokens of 2 or more alphanumeric characters (punctuation is completely
+        ignored and always treated as a token separator).
+    ngram_range : tuple (min_n, max_n)
+        The lower and upper boundary of the range of n-values for different n-grams
+        to be extracted. All values of n such that min_n <= n <= max_n will be used.
+        (default is (1, 1))
+    max_df : float in range [0.0, 1.0] or int
+        When building the vocabulary ignore terms that have a document frequency strictly
+        higher than the given threshold (corpus-specific stop words). If float, the parameter
+        represents a proportion of documents, integer absolute counts. This parameter is
+        ignored if vocabulary is not None. (default is 1.0)
+    min_df : float in range [0.0, 1.0] or int
+        When building the vocabulary ignore terms that have a document frequency
+        strictly lower than the given threshold. This value is also called cut-off
+        in the literature. If float, the parameter represents a proportion of
+        documents, integer absolute counts. This parameter is ignored if vocabulary
+        is not None. (default is 1)
+    vocabulary : Mapping or iterable, optional
+        Either a Mapping (e.g., a dict) where keys are terms and values are indices
+        in the feature matrix, or an iterable over terms. If not given, a vocabulary
+        is determined from the input documents. (default is None)
     paragraphs : iterable
         an iterable which yields either str, unicode or file objects
     top_n : int
@@ -53,25 +87,44 @@ class TfidfRetriever(BaseEstimator):
     """
 
     def __init__(self,
+                 lowercase=True,
+                 preprocessor=None,
+                 tokenizer=None,
+                 stop_words='english',
+                 token_pattern=r"(?u)\b\w\w+\b",
                  ngram_range=(1, 2),
                  max_df=0.85,
-                 stop_words='english',
+                 min_df=2,
+                 vocabulary=None,
                  paragraphs=None,
                  top_n=3,
                  verbose=False):
 
+        self.lowercase = lowercase
+        self.preprocessor = preprocessor
+        self.tokenizer = tokenizer
+        self.stop_words = stop_words
+        self.token_pattern = token_pattern
         self.ngram_range = ngram_range
         self.max_df = max_df
-        self.stop_words = stop_words
+        self.min_df = min_df
+        self.vocabulary = vocabulary
         self.paragraphs = paragraphs
         self.top_n = top_n
         self.verbose = verbose
 
     def fit(self, X, y=None):
 
-        self.vectorizer = TfidfVectorizer(ngram_range=self.ngram_range,
+        self.vectorizer = TfidfVectorizer(lowercase=self.lowercase,
+                                          preprocessor=self.preprocessor,
+                                          tokenizer=self.tokenizer,
+                                          stop_words=self.stop_words,
+                                          token_pattern=self.token_pattern,
+                                          ngram_range=self.ngram_range,
                                           max_df=self.max_df,
-                                          stop_words=self.stop_words)
+                                          min_df=self.min_df,
+                                          vocabulary=self.vocabulary
+                                          )
         self.tfidf_matrix = self.vectorizer.fit_transform(X)
 
         return self
